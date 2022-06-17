ae6d16015e716250d421af4dce33722da0841a36
Gabriel Bovi Lorandi <gabriel.bovi96@gmail.com>
2022-06-17 16:11:17 -0300

    Adds initial project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# book-library
+
+API for managing library books inventory.
---
+++ src/test/java/com/gabriellorandi/booklibrary/BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellorandi.booklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
